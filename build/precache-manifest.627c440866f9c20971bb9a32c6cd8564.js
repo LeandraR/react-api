f0965149c711d0b57b6b04a793f05cb430f6733b
leandra <leandra.reid@gmail.com>
2018-10-29 18:23:18 -0400

    changing page title

---
+++ build/precache-manifest.627c440866f9c20971bb9a32c6cd8564.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5fc6502f583e3133ff6",
+    "url": "/react-api/static/js/runtime~main.d5fc6502.js"
+  },
+  {
+    "revision": "ce3c7df3f0373b419ec9",
+    "url": "/react-api/static/js/main.ce3c7df3.chunk.js"
+  },
+  {
+    "revision": "c1e6fdeb6660648b6cdf",
+    "url": "/react-api/static/js/1.c1e6fdeb.chunk.js"
+  },
+  {
+    "revision": "ce3c7df3f0373b419ec9",
+    "url": "/react-api/static/css/main.6a900209.chunk.css"
+  },
+  {
+    "revision": "8561e6589bae86ca308438d38dce2120",
+    "url": "/react-api/index.html"
+  }
+];
